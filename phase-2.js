9dfe3c591eb8aaf6d67eeae0901cf6cd78e77d8f
Ethan Julius <56324629+500EJ@users.noreply.github.com>
2022-08-27 11:39:48 -0400

    Implement addNums10 and addManyNums10 (Phase 2)

--- phase-2.js
+++ phase-2.js
@@ -2,12 +2,20 @@ const [addNums, addManyNums] = require("./phase-1");
 
 // Runs `addNums` in 10 increasing increments
 function addNums10(increment) {
-  // Fill this in
+  const sums = [];
+  for (let i = increment; i <= increment * 10; i += increment) {
+    sums.push(addNums(i));
+  }
+  return sums;
 }
 
 // Runs `addManyNums` in 10 increasing increments
 function addManyNums10(increment) {
-  // Fill this in
+  const sums = [];
+  for (let i = increment; i <= increment * 10; i += increment) {
+    sums.push(addManyNums(i));
+  }
+  return sums;
 }
 
 module.exports = [addNums10, addManyNums10];
